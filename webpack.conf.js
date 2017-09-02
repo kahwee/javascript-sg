bf077a59a33c6b8ca442f11de99d51ffedfd534b
KahWee Teng <t@kw.sg>
2017-09-02 09:36:45 -0700

    Cleanup gulp

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,16 +1,19 @@
-import webpack from "webpack";
-import path from "path";
+const webpack = require('webpack')
+const path = require('path')
 
-export default {
+module.exports = {
   module: {
     loaders: [
       {
         test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file?name=/[hash].[ext]"
+        loader: 'file?name=/[hash].[ext]'
       },
-      {test: /\.json$/, loader: "json-loader"},
       {
-        loader: "babel",
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        loader: 'babel',
         test: /\.js?$/,
         exclude: /node_modules/,
         query: {cacheDirectory: true}
@@ -20,19 +23,19 @@ export default {
 
   plugins: [
     new webpack.ProvidePlugin({
-      "fetch": "imports?this=>global!exports?global.fetch!whatwg-fetch"
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
     })
   ],
 
-  context: path.join(__dirname, "src"),
+  context: path.join(__dirname, 'src'),
   entry: {
-    app: ["./js/app"],
-    cms: ["./js/cms"]
+    app: ['./js/app'],
+    cms: ['./js/cms']
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "[name].js"
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js'
   },
-  externals:  [/^vendor\/.+\.js$/]
-};
+  externals: [/^vendor\/.+\.js$/]
+}
